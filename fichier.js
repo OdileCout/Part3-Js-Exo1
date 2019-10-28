8fd6c320db1836688a151ee7f02f79de3014998d
Votre Nom <odilecoutiez@gmail.com>
2019-10-28 14:45:30 +0100

    Exercice1 Js Part-3

---
+++ fichier.js
@@ -0,0 +1,8 @@
+function change(){
+  //declarer une fonction de changer le style de mon imageen ajoutant un border au survol
+  document.getElementById('image1').style.border = '3px inset red';
+}
+function retu(){
+  //declarer une fonction de changer le style de mon imageen afin de retirer le border en retirant la souris
+  document.getElementById('image1').style.border = 'none';
+}
